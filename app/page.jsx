dfcd420b9d91908c6b346b5222e1cec6332a96a5
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2024-09-01 11:10:50 +0530

    added sorting and searching

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="">
       <h1 className="text-5xl my-4 px-4 text-center">All Prompts</h1>
-      <p className="text-xl my-4 px-4 text-center">Explore Swanirbhar's AI Prompts</p>
+      <p className="text-xl my-4 px-4 text-center">Explore Swanirbhar AI Prompts</p>
         <AllRoutes/>
     </div>
 
